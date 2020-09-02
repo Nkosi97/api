ac31ed41d83394e84376d5c59c90e87f62f9735d
Nkosi97 <nkosinathisiyeni97@gmail.com>
2020-09-02 11:53:29 +0200

    fixed src

--- spec/api_spec.js
+++ spec/api_spec.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 describe("fetchUserData function", () => {
     it(`should fetch user data from api`, () => {
-      expect(fetch('https://api.github.com/repos/Nkosi97/level1programming')).toBe()
+      expect(fetch('https://api.github.com/repos/Nkosi97/level1programming')).toBe(res)
     });
 
    
--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-function fetchUserData(){
+// function fetchUserData(){
 
-    // const url = 'https://api.github.com/repos/Nkosi97/level1programming'
-    const fetch = require("node-fetch");
+//     // const url = 'https://api.github.com/repos/Nkosi97/level1programming'
+//     const fetch = require("node-fetch");
     
-    fetch('https://api.github.com/users/Nkosi97')
+//     fetch('https://api.github.com/users/Nkosi97')
 
-      .then(res => {
-        if (res.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return res.json().followers;
-      })
-      .then(user => {
-        console.log(user);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+//       .then(res => res.json())
+//       .then(user => {
+//         console.log(user);
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       });
+// }
+
+// fetchUserData()
+const fetch = require("node-fetch");
+function api(repoName){
+  fetch(`https://api.github.com/repos/${repoName}/commits/6cdecd2057c843ee86831641df7a05bf47735f38`)
+    .then(res => res.json())
+    .then(json => console.log(json));
+
 }
-fetchUserData()
+api("Nkosi97/level1programming")
 
-module.export = {fetchUserData}
+// module.export = {fetchUserData}
